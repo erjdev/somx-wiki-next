5e5bbd546d943bb601acf22401b04be032865c82
Erik <erik@boxhammer.dev>
2023-04-30 00:50:41 -0400

    Update ring.tsx

--- src/components/ring.tsx
+++ src/components/ring.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { SomRing } from '../../somdata/types/item'
 import { StatsBlock } from './stat-block';
+import Link from 'next/link';
 
 const Ring: React.FC<{ ring: SomRing }> = ({ ring }) => {
   return (
-    <div className="flex flex-col p-4 gap-3 max-w-xl">
+    <div className="flex flex-col p-4 gap-3 max-w-xl rounded-lg">
       <div className="flex items-center gap-4 flex-grow-0">
         <img className="w-12 h-12" src={ring.imageUrl} alt={ring.name} />
         <div className="flex flex-col gap-1">
-          <h2 className="text-xl underline underline-offset-4 decoration-blue-700">{ring.name}</h2>
+          <Link className="cursor-pointer" href={`/rings/${ring.id}`}>
+            <p className="text-xl underline underline-offset-4 decoration-blue-700">{ring.name}</p>
+          </Link>
           <div className="flex justify-between italic font-light gap-2 items-center text-sm">
             {ring.requiredStats?.level && <p className="opacity-80 p-1 pl-0">Required Level: {ring.requiredStats?.level}</p>}
             {ring.unique && <p className="rounded-full bg-slate-800 px-3 tracking-wider text-sm opacity-80">Unique</p>}
