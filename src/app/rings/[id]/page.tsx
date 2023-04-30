a5cd7b85e5faf2bbc5178eacdbef861cb2dd50ae
Erik <erik@boxhammer.dev>
2023-04-29 22:30:34 -0400

    Updated ring page

--- src/app/rings/[id]/page.tsx
+++ src/app/rings/[id]/page.tsx
@@ -13,7 +13,13 @@ export default async function RingPage({ params }: { params: { id: string } }) {
     <div className="flex flex-col mx-auto p-4 gap-2">
       <div className="flex items-center gap-2">
         <img className="w-12 h-12" src={ring.imageUrl} alt={ring.name} />
-        <h2 className="text-xl">{ring.name}</h2>
+        <div className="flex flex-col">
+          <h2 className="text-xl">{ring.name}</h2>
+          <div className="flex justify-between italic font-light gap-4">
+            {ring.requiredStats?.level && <p>Required Level: {ring.requiredStats?.level}</p>}
+            {ring.unique && <p>Unique</p>}
+          </div>
+        </div>
       </div>
       <p>{ring.description}</p>
       <p>{ring.availableClasses}</p>
