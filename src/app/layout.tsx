b1e1269ce3bf55839ffb4191ce7b8ba0780a4995
Erik <erik@boxhammer.dev>
2023-04-30 14:46:30 -0400

    Centered main layout, added max width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-slate-950 text-slate-50`}>
-        <Navbar />
-        {children}
+        <div className="max-w-7xl mx-auto">
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   )
