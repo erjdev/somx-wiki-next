effe5d80cc69cd699341426cf1886839799a9d5b
Erik <erik@boxhammer.dev>
2023-04-29 23:08:25 -0400

    Improved ring view

--- src/app/rings/[id]/page.tsx
+++ src/app/rings/[id]/page.tsx
@@ -10,12 +10,12 @@ export default async function RingPage({ params }: { params: { id: string } }) {
   const ring = await getRingAsync(params.id);
 
   return (
-    <div className="flex flex-col mx-auto p-4 gap-2">
+    <div className="flex flex-col mx-auto p-4 gap-2 max-w-xl">
       <div className="flex items-center gap-2">
         <img className="w-12 h-12" src={ring.imageUrl} alt={ring.name} />
         <div className="flex flex-col">
-          <h2 className="text-xl">{ring.name}</h2>
-          <div className="flex justify-between italic font-light gap-4">
+          <h2 className="text-xl underline underline-offset-4 decoration-blue-700">{ring.name}</h2>
+          <div className="flex justify-between italic font-light gap-12">
             {ring.requiredStats?.level && <p>Required Level: {ring.requiredStats?.level}</p>}
             {ring.unique && <p>Unique</p>}
           </div>
--- src/components/stat-block.tsx
+++ src/components/stat-block.tsx
@@ -1,17 +1,18 @@
 import { SomStats } from "../../somdata/types/base-data";
 
+
 const StatIcon: React.FC<{positive: boolean}> = ({ positive }) => {
   if (!positive) {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
     )
   }
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
   )
 }
@@ -25,9 +26,9 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
   const negativeColorClass = "text-red-300";
 
   return (
-    <div className="flex flex-col p-4 gap-2">
+    <div className="flex flex-col gap-2 max-w-sm">
       <div className="flex items-center gap-2">
-        <p className="text-xl">{title ?? 'Stats'}</p>
+        <p className="text-md tracking-wide opacity-80">{title ?? 'Stats'}</p>
       </div>
 
       <div className="grid grid-cols-2 gap-2">
@@ -37,7 +38,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.health > 0} />
               Health
             </p>
-            <p className={statBlock.health > 0 ? positiveColorClass : negativeColorClass}>{statBlock.health}</p>
+            <p className={`text-right ${statBlock.health > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.health}</p>
           </>
         )}
         {statBlock.mana && (
@@ -46,7 +47,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.mana > 0} />
               Mana
             </p>
-            <p className={statBlock.mana > 0 ? positiveColorClass : negativeColorClass}>{statBlock.mana}</p>
+            <p className={`text-right ${statBlock.mana > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.mana}</p>
           </>
         )}
         {statBlock.strength && (
@@ -55,7 +56,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.strength > 0} />
               Strength
             </p>
-            <p className={statBlock.strength > 0 ? positiveColorClass : negativeColorClass}>{statBlock.strength}</p>
+            <p className={`text-right ${statBlock.strength > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.strength}</p>
           </>
         )}
         {statBlock.defense && (
@@ -64,7 +65,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.defense > 0} />
               Defense
             </p>
-            <p className={statBlock.defense > 0 ? positiveColorClass : negativeColorClass}>{statBlock.defense}</p>
+            <p className={`text-right ${statBlock.defense > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.defense}</p>
           </>
         )}
         {statBlock.magic && (
@@ -73,7 +74,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.magic > 0} />
               Magic
             </p>
-            <p className={statBlock.magic > 0 ? positiveColorClass : negativeColorClass}>{statBlock.magic}</p>
+            <p className={`text-right ${statBlock.magic > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.magic}</p>
           </>
         )}
         {statBlock.speed && (
@@ -82,7 +83,7 @@ export const StatsBlock = ({ title, statBlock }: { statBlock: SomStats, title?: 
               <StatIcon positive={statBlock.speed > 0} />
               Speed
             </p>
-            <p className={statBlock.speed > 0 ? positiveColorClass : negativeColorClass}>{statBlock.speed}</p>
+            <p className={`text-right ${statBlock.speed > 0 ? positiveColorClass : negativeColorClass}`}>{statBlock.speed}</p>
           </>
         )}
       </div>
