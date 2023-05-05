f59a922dba2685f701d42b6d578d426336f4d8ce
Erik <erik@boxhammer.dev>
2023-05-05 01:16:43 -0400

    Updated metadata for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: 'SoMX Wiki',
+  description: 'Wiki for Secrets of Mirage',
+};
+
 export default async function Home() {
   return (
     <div>
