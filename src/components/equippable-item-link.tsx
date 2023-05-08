82a483745fc9c0426fac6d10c9be1e4108a27ff5
Erik <erik@boxhammer.dev>
2023-05-08 19:24:05 -0400

    Updated item links to be cleaner and consistent

--- src/components/equippable-item-link.tsx
+++ src/components/equippable-item-link.tsx
@@ -18,16 +18,18 @@ const EquippableItemLink: React.FC<React.PropsWithChildren<{
   return (
     <Link
       href={href}
-      className={`flex items-center gap-4 p-2 flex-grow-0 rounded-lg bg-white/5 hover:bg-white/10 text-xs`}>
-      {imageUrl && <img src={imageUrl} alt={name} className="w-12 h-12" />}
-      <div className="flex-grow flex flex-col gap-1">
-        <p className={`text-xl underline underline-offset-4 ${underlineAccentClass} group-hover:decoration-blue-400`}>{name}</p>
-        <div className="flex flex-wrap justify-between italic font-light gap-4 items-center">
-          {level && <p className="text-blue-400">Level {level}</p>}
-          <div className="flex flex-wrap gap-x-4 items-center">
-            {children}
+      className={`flex flex-col gap-4 p-3 flex-grow-0 rounded-lg bg-white/5 hover:bg-white/10 text-xs`}>
+      <div className="flex flex-row gap-3">
+        {imageUrl && <img src={imageUrl} alt={name} className="flex-shrink-0 w-12 h-12" />}
+        <div className="flex-grow flex flex-col gap-1">
+          <p className={`text-xl underline underline-offset-4 ${underlineAccentClass} group-hover:decoration-blue-400`}>{name}</p>
+          <div className="flex flex-wrap justify-between italic font-light gap-4 items-center">
+            {level && <p className="text-blue-400">Level {level}</p>}
           </div>
         </div>
+      </div>
+      <div className="flex flex-wrap gap-x-4 justify-center items-center">
+        {children}
       </div>
     </Link>
   )
