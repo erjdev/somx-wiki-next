3e0186203a11ca1d63bb9ab114ee10416dc54315
Erik <erik@boxhammer.dev>
2023-05-08 20:20:27 -0400

    Updated main layout to be centered

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           <main className="flex-grow flex flex-col items-center justify-between px-8 pb-8">
             {children}
           </main>
-          <footer className="mx-auto flex flex-col items-center font-extralight text-xs tracking-wider text-slate-400 gap-1 pb-2">
+          <footer className="mx-auto flex flex-col text-center items-center font-extralight text-xs tracking-wider text-slate-400 gap-1 pb-2">
             <div>Made with ❤️ by <span className="font-medium">Slate</span></div>
             <div>Images and items from <a className="text-blue-400" href="https://www.secretsofmirage.com/" target="_blank" rel="noreferrer noopener">Secrets of Mirage (SoMX)</a></div>
           </footer>
